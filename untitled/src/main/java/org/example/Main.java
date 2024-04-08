e74035276b92d760048e94c1cb01651c22a6e3a9
FaezehKhoshayand <faezehkhoshayand@gmail.com>
2024-04-08 20:46:56 +0330

    login & signup

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Reddit reddit = new Reddit();
+        Reddit.addAccount(new Account("qw", "qw","qwerty@gmail.com"));
         runMenu();
     }
     public static void runMenu() {
         while (true) {
             System.out.println("Enter your command\n1-Login 2-Sign up 3-Exit");
             Scanner in = new Scanner(System.in);
-            switch (in.nextInt()) {
+            int cmd = in.nextInt();
+            switch (cmd) {
                 case 1:
                     login();
                     break;
@@ -25,21 +27,53 @@ public class Main {
         }
     }
     public static void login() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("username: ");
-        Scanner p = new Scanner(System.in);
-        String username = p.nextLine();
+        String username = scanner.nextLine();
         System.out.println("password: ");
-        Scanner i = new Scanner(System.in);
-        String password = i.nextLine();
+        scanner = new Scanner(System.in);
+        String password = scanner.nextLine();
         System.out.println("EmailAddress: ");
-        Scanner o = new Scanner(System.in);
-        String emailAddress = o.nextLine();
+        scanner = new Scanner(System.in);
+        String emailAddress = scanner.nextLine();
         Account account = new Account(username, password, emailAddress);
-        // account = account.login();
-        System.out.println("TimeLine:");
+        account = account.login();
+        if(account == null) {
+            System.out.println("Such an account does not exist 1)login 2)Sign up");
+            Scanner in = new Scanner(System.in);
+            int cmd = in.nextInt();
+            switch(cmd) {
+                case 1:
+                    login();
+                    break;
+                case 2:
+                    signUp();
+                    break;
+                default:
+                    login();
+            }
+        }
+        else {
+            System.out.println("1)View TimeLine  2)View Joined Subreddits  3)View All Subreddits  4)View Profile  5)Create Subreddit");
+        }
         //timeline
     }
     public static void signUp() {
-        //reddit.viewAllSubreddits();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("username: ");
+        String username = scanner.nextLine();
+        System.out.println("password: ");
+        scanner = new Scanner(System.in);
+        String password = scanner.nextLine();
+        System.out.println("EmailAddress: ");
+        scanner = new Scanner(System.in);
+        String emailAddress = scanner.nextLine();
+        while(!Account.validateEmail(emailAddress)) {
+            System.out.println("Not a valid emailAddress\nNew EmailAddress: ");
+            scanner = new Scanner(System.in);
+            emailAddress = scanner.nextLine();
+        }
+        Account account = new Account(username, password, emailAddress);
+        account.signup(account);
     }
 }
